474f675ea9b24c5ab23fe18b3eb13a9095b790c1
Mihai <amihaiemil@gmail.com>
2020-02-04 18:26:00 +0200

    Update indexPostsWithTag.js

--- _includes/indexPostsWithTag.js
+++ _includes/indexPostsWithTag.js
@@ -4,6 +4,10 @@
     This Index should be used for search on a given Tag page.
     The search will be performed only through the posts which are marked with said tag.
     
+    Furthermore, search should be performed only through titles and previews, to make things lighter.
+    If we are on a Tag page, chances are users are interested in posts with the same tag, thus search
+    by title and preview text is more suitable.
+    
     HINT: include this script at the end of the page so that, if the indexing takes
     longer, it won't affect the displaying of the page.
 -->
@@ -14,7 +18,6 @@
         function () {
             this.addField('title');
             this.addField('preview');
-            this.addField('content');
             this.addField('date');
             this.setRef('id');
         }
@@ -24,7 +27,6 @@
         var postToIndex = {
           "id": id++,
           "title": "{{ post.title | replace: '"', '\"' }}",
-          "content": "{{ post.content | replace: '"', '\"' }}",
           "preview": "{{ post.preview | replace: '"', '\"' }}",
           "date": "{{ post.date }}"
         }
